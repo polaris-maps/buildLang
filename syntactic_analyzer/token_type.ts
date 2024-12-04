d1d90a1a73ae49e17a1a00a6d75916e331ab4290
christineiym <mendoza@cmu.edu>
2024-12-04 09:31:26 -0500

    convert tokentype to javascript

---
+++ syntactic_analyzer/token_type.ts
@@ -0,0 +1,39 @@
+enum TokenType {
+    BUILDING_TYPE,
+    FLOOR_TYPE,
+    LBRACE,
+    RBRACE,
+    LPAREN,
+    RPAREN,
+    COMMA,
+    PERIOD,
+    SEMICOLON,
+    HALLWAY,
+    ASSIGN,
+    OPEN,
+    RANGE,
+    INT_LITERAL,
+    BOOLEAN_LITERAL,
+    TIME_HH_MM_LITERAL,
+    EOT,
+    ERROR
+}
+
+//     BUILDING_TYPE = "building"
+//     FLOOR_TYPE = "floor"
+//     LBRACE = "{"
+//     RBRACE = "}"
+//     LPAREN = "("
+//     RPAREN = ")"
+//     COMMA = ","
+//     PERIOD = "."
+//     SEMICOLON = ";"
+//     HALLWAY = "|"  # write something else
+//     ASSIGN = "="
+//     OPEN = "open"
+//     RANGE = "-"
+//     INT_LITERAL = 0
+//     BOOLEAN_LITERAL = 1
+//     TIME_HH_MM_LITERAL = 2
+//     EOT = 3
+//     ERROR = 4
