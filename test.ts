386937025929473808a704409158da37d116ae56
christineiym <mendoza@cmu.edu>
2024-12-04 12:10:09 -0500

    add node dependency and some test logging

--- test.ts
+++ test.ts
@@ -1,2 +1,18 @@
+import { ErrorReporter } from "./ErrorReporter";
+import { InputStream } from "./InputStream";
+import { Scanner } from "./SyntacticAnalyzer/Scanner";
+
 // read in file
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./sn_fb.bl', 'utf-8');
+
 // parse file
+const myInputStream = new InputStream(file);
+const myErrorReporter = new ErrorReporter();
+const myScanner = new Scanner(myInputStream, myErrorReporter);
+var i = 0
+while(i < 100){
+    console.log(myScanner.scan());
+    i++;
+}
