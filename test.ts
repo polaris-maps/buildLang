1bfe408a6597c0027b8044aeef2a6406b073f96a
christineiym <mendoza@cmu.edu>
2024-12-11 13:56:29 -0500

    sn/fb floor 0 test

--- test.ts
+++ test.ts
@@ -9,7 +9,8 @@ import { SQLGenerator } from "./CodeGeneration/SQLGenerator";
 
 // read in file
 // const filePath = './sn_fb_room_code.bl';
-const filePath = './small.bl';
+const filePath = './sn_fb_global_rooms.bl';
+// const filePath = './small.bl';
 const file = readFileSync(filePath, 'utf-8');
 const myInputStream = new InputStream(file);
 
